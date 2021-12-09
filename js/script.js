b81e1d5a76a0a0da1365838a5023a3ab8051d4e5
ElizabethPeredo <61327189+ElizabethPeredo@users.noreply.github.com>
2021-12-09 03:57:35 -0500

    2nd files

--- js/script.js
+++ js/script.js
@@ -5,3 +5,18 @@ menu.onclick = () => {
     menu.classList.toggle("fa-times");
     navbar.classList.toggle("active");
 }
+
+let menu_inversion1 = document.querySelector("#menu-inversion1");
+let menu_inversion2 = document.querySelector("#menu-inversion2");
+
+let inversion1 = document.querySelector("#inversion1");
+let inversion2 = document.querySelector("#inversion2");
+
+menu_inversion1.onclick = () => {
+    inversion1.classList.remove("invisible");
+    inversion1.classList.toggle("visible");
+}
+menu_inversion2.onclick = () => {
+    inversion2.classList.remove("invisible");
+    inversion2.classList.toggle("visible");
+}
