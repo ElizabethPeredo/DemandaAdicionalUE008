6177fea4fd9ab7c0096216761a7cddfa3c063e5d
ElizabethPeredo <61327189+ElizabethPeredo@users.noreply.github.com>
2021-12-16 18:38:44 -0500

    Update

---
+++ js/page_inv.js
@@ -0,0 +1,23 @@
+document.getElementById("header").innerHTML =
+    '<div class="navbar__container">' +
+    '<div class="navbar__logo">' +
+    '<a href="https://elizabethperedo.github.io/DemandaAdicionalUE008/">' +
+    '<i class="">' +
+    '</i>Unidad Ejecutora 008</a >' +
+    '</div >' +
+    '<div class="navbar__items">' +
+    '<ul>' +
+    '<a href="https://elizabethperedo.github.io/DemandaAdicionalUE008/">Home</a>' +
+    '<a href="../anexo1.html">Anexo 1</a>' +
+    '<a href="../anexo2.html">Anexo 2</a>' +
+    '<a href="../anexo3.html">Anexo 3</a>' +
+    '<a href="../anexo4.html">Anexo 4</a>' +
+    '<a href="../anexo5.html">Anexo 5</a>' +
+    '<a href="../objetoDA.html">Objeto DA</a>' +
+    '</ul>' +
+    '</div>' +
+    '<div class="navbar__icon">' +
+    '<i class="fas fa-bars" id="menu-bars"></i>' +
+    '<i class="fas fa-search" id="menu-search"></i>' +
+    '</div>' +
+    '</div >';
